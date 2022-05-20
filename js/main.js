4f1a7fa89748948f8955058c53dc763f0cebe6a7
Sam Saryon <ssaryonjr@gmail.com>
2022-05-19 18:50:05 -0700

    added customer review slider

--- js/main.js
+++ js/main.js
@@ -212,3 +212,44 @@ const swiper = new Swiper('.swiper', {
 	  el: '.swiper-scrollbar',
 	},
   });
+
+
+
+  const testimony = new Swiper('.testimony-carousel', {
+	// Optional parameters
+	direction: 'horizontal',
+	loop: true,
+	autoplay: true,
+  
+	// If we need pagination
+	pagination: {
+	  el: '.swiper-pagination',
+	},
+  
+	// And if we need scrollbar
+	scrollbar: {
+	  el: '.swiper-scrollbar',
+	},
+  
+	// Default parameters
+	slidesPerView: 1,
+	spaceBetween: 10,
+	// Responsive breakpoints
+	breakpoints: {
+	  // when window width is >= 320px
+	  320: {
+		slidesPerView: 1,
+		spaceBetween: 20
+	  },
+	  // when window width is >= 480px
+	  768: {
+		slidesPerView: 2,
+		spaceBetween: 30
+	  },
+	  // when window width is >= 640px
+	  993: {
+		slidesPerView: 3,
+		spaceBetween: 40
+	  }
+	}
+  });
